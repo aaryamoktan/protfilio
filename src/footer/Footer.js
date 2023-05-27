18ff91601ad829325ade96cbb9dba36e95f2a7af
aarya moktan <aaryamoktan2055@gmail.com>
2023-05-27 01:14:04 -0700

    first file

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,12 +6,12 @@ const Footer = () => {
             <div className='footer-main-container'>
                 <div class="footer-cont">
                     <h3>Follow me on These</h3>
-                    <a href="https://www.facebook.com/aarya.moktan.98/" >Facebook</a>
-                    <a href="https://www.instagram.com/aaryan_moktan/">Instagram</a>
-                    <a href="https://www.linkedin.com/in/aaryan-moktan-872152273/">Linkedin</a>
+                    <a href="https://www.facebook.com/aarya.moktan.98/" target="blank">Facebook</a>
+                    <a href="https://www.instagram.com/aaryan_moktan/" target="blank">Instagram</a>
+                    <a href="https://www.linkedin.com/in/aaryan-moktan-872152273/" target="blank">Linkedin</a>
                 </div>
                 <div className='email-para'>
-               <p > Email: <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=new">aaryamoktan2055@gmail.com</a></p></div>
+               <p > Email: <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=new" target="blank">aaryamoktan2055@gmail.com</a></p></div>
             </div>
         </>
     )
